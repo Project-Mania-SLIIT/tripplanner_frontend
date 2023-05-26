4c9661da7a2a65c9993de6caa4b7ffaf1da85c01
Mohammed Aathif <IT20644680@my.sliit.lk>
2023-05-26 18:09:58 +0530

    fixes ui

--- src/pages/user/profile/index.jsx
+++ src/pages/user/profile/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import Navbar from "../../../components/navbar";
 import Footer from "../../../components/footer";
 import axios from "axios";
@@ -51,113 +51,111 @@ export default function Profile() {
   return (
     <div>
       <Navbar />
-      <div className="row">
-        <div>
-          <div className="container m-5">
-            <div className="row flex-lg-nowrap">
-              <div className="col">
-                <div className="row">
-                  <div className="col mb-3">
-                    <div className="trow d-flex justify-content-center">
-                      <div className="card" style={{ width: "40rem" }}>
-                        <div className="card-body">
-                          <div className="e-profile">
-                            <div className="row">
-                              <div className="col-12 col-sm-auto mb-3">
+      <div>
+        <div className="container p-5 ">
+          <div className="row flex-lg-nowrap">
+            <div className="col">
+              <div className="row">
+                <div className="col mb-3">
+                  <div className="trow d-flex justify-content-center">
+                    <div className="card" style={{ width: "40rem" }}>
+                      <div className="card-body">
+                        <div className="e-profile">
+                          <div className="row">
+                            <div className="col-12 col-sm-auto mb-3">
+                              <div
+                                className="mx-auto"
+                                style={{ width: "140px" }}
+                              >
                                 <div
-                                  className="mx-auto"
-                                  style={{ width: "140px" }}
+                                  className="d-flex justify-content-center align-items-center rounded"
+                                  style={{
+                                    height: "140px",
+                                    backgroundColor: "rgb(233, 236, 239)",
+                                  }}
                                 >
-                                  <div
-                                    className="d-flex justify-content-center align-items-center rounded"
+                                  <span
                                     style={{
-                                      height: "140px",
-                                      backgroundColor: "rgb(233, 236, 239)",
+                                      color: "rgb(166, 168, 170)",
+                                      font: "bold 8pt Arial",
                                     }}
                                   >
-                                    <span
-                                      style={{
-                                        color: "rgb(166, 168, 170)",
-                                        font: "bold 8pt Arial",
-                                      }}
-                                    >
-                                      140x140
-                                    </span>
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="col d-flex flex-column flex-sm-row justify-content-between mb-3">
-                                <div className="text-center text-sm-left mb-2 mb-sm-0">
-                                  <h3 className="pt-sm-3 pb-1 mb-0 text-nowrap">
-                                    Welcome! {username} @{role}
-                                  </h3>
-                                  <h4 className="mb-0">
-                                    A Destination For The New Millennium Begins
-                                    Here.
-                                  </h4>
+                                    140x140
+                                  </span>
                                 </div>
                               </div>
                             </div>
-                            <ul className="nav nav-tabs">
-                              <li className="nav-item">
-                                <div className="active nav-link">
-                                  User Details
-                                </div>
-                              </li>
-                            </ul>
-                            <div className="tab-content pt-3">
-                              <div className="tab-pane active">
-                                <form className="form" noValidate>
-                                  <div className="row">
-                                    <div className="col- col-sm-12 mb-3">
-                                      <div className="row">
-                                        <div className="col">
-                                          <div className="form-group">
-                                            <label>Username</label>
-                                            <input
-                                              className="form-control"
-                                              type="text"
-                                              name="username"
-                                              value={username}
-                                              readonly
-                                            />
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <div className="row">
-                                    <div className="col- col-sm-12 mb-3">
-                                      <div className="row">
-                                        <div className="col">
-                                          <div className="form-group">
-                                            <label>Email</label>
-                                            <input
-                                              className="form-control"
-                                              type="text"
-                                              value={email}
-                                            />
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-
-                                  <div className="row">
-                                    <div className="col d-flex justify-content-end">
-                                      <UpdateProfile usid={user} />
-                                      <Button
-                                        className="btn btn-danger ms-1"
-                                        onClick={() => {
-                                          handledelete(user);
-                                        }}
-                                      >
-                                        Delete Account
-                                      </Button>
-                                    </div>
-                                  </div>
-                                </form>
+                            <div className="col d-flex flex-column flex-sm-row justify-content-between mb-3">
+                              <div className="text-center text-sm-left mb-2 mb-sm-0">
+                                <h3 className="pt-sm-3 pb-1 mb-0 text-nowrap">
+                                  Welcome! {username} @{role}
+                                </h3>
+                                <h4 className="mb-0">
+                                  A Destination For The New Millennium Begins
+                                  Here.
+                                </h4>
                               </div>
+                            </div>
+                          </div>
+                          <ul className="nav nav-tabs">
+                            <li className="nav-item">
+                              <div className="active nav-link">
+                                User Details
+                              </div>
+                            </li>
+                          </ul>
+                          <div className="tab-content pt-3">
+                            <div className="tab-pane active">
+                              <form className="form" noValidate>
+                                <div className="row">
+                                  <div className="col- col-sm-12 mb-3">
+                                    <div className="row">
+                                      <div className="col">
+                                        <div className="form-group">
+                                          <label>Username</label>
+                                          <input
+                                            className="form-control"
+                                            type="text"
+                                            name="username"
+                                            value={username}
+                                            readonly
+                                          />
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="row">
+                                  <div className="col- col-sm-12 mb-3">
+                                    <div className="row">
+                                      <div className="col">
+                                        <div className="form-group">
+                                          <label>Email</label>
+                                          <input
+                                            className="form-control"
+                                            type="text"
+                                            value={email}
+                                          />
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+
+                                <div className="row">
+                                  <div className="col d-flex justify-content-end">
+                                    <UpdateProfile usid={user} />
+                                    <Button
+                                      className="btn btn-danger ms-1"
+                                      onClick={() => {
+                                        handledelete(user);
+                                      }}
+                                    >
+                                      Delete Account
+                                    </Button>
+                                  </div>
+                                </div>
+                              </form>
                             </div>
                           </div>
                         </div>
@@ -170,7 +168,9 @@ export default function Profile() {
           </div>
         </div>
       </div>
+      <div className="fixed-bottom"> 
       <Footer />
+      </div>
     </div>
   );
 }

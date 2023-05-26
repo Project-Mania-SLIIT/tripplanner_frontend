df108be9a25e666533b49945fa1d830da4e04111
Mohammed Aathif <IT20644680@my.sliit.lk>
2023-05-26 18:55:47 +0530

    adminplace completed

--- src/pages/admin/place/index.jsx
+++ src/pages/admin/place/index.jsx
@@ -2,46 +2,98 @@ import React, { useEffect, useState } from "react";
 import Sidenav from "../../../components/admin/sidenav";
 import { Button, Table } from "react-bootstrap";
 import axios from "axios";
-import UpdUserModal from "../../../components/modals/User/UpdUserModal";
 import Swal from "sweetalert2";
+import AddPlaceModal from "../../../components/modals/Place/addPlaceModal";
+import UpdatePlaceModal from "../../../components/modals/Place/updatePlaceModal";
 
-export default function index() {
+export default function Index() {
+  const [blog, setBlog] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/v1/blog")
+      .then((res) => {
+        setBlog(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function handledelete(id) {
+    axios
+      .delete("http://localhost:4000/api/v1/blog/" + id)
+      .then(function (response) {
+        Swal.fire({
+          title: "Success!",
+          text: "Blog Deleted Successfully",
+          icon: "success",
+        }).then(() => {
+          window.location.reload();
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <div>
-      <Sidenav/>
-      <br/><div className="text-center">
-            <br />
-            <h1>Places</h1>
-            <br />
-            {/* <AddHotelModal/> */}
-            <br />
-            <br />
-            <Table striped>
-              <thead>
-                <tr>
-                  <th>Hotel Name</th>
-                  <th>Description</th>
-                  <th>City</th>
-                  <th>Image</th>
-                  <th>Room Count</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* {hotel.map((ht) => (
-                  <tr key={ht._id}>
-                    <td>{ht.name}</td>
-                    <td>{ht.description}</td>
-                    <td><img src={ht.image} style={{ width: "50px", height:"50px" }} alt="pic"/></td>
-                    <td>{ht.city}</td>
-                    <td>{ht.roomCount}</td>
-                    <td>
-                                    </td> 
-                  </tr>
-                ))} */}
-              </tbody>
-            </Table>
-          </div>
+      <Sidenav />
+      <br />
+      <div className="text-center">
+        <br />
+        <h1>Culture and Heritage Tour Places</h1>
+        <br />
+        <AddPlaceModal />
+        <br />
+        <br />
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Description</th>
+              <th>City</th>
+            </tr>
+          </thead>
+          <tbody>
+            {blog.map((blog) => (
+              <tr key={blog._id}>
+                <td>
+                  <img
+                    src={blog.image}
+                    style={{ width: "50px", height: "50px" }}
+                    alt="picture"
+                    href={blog.image}
+                  />
+                </td>
+                <td>{blog.title}</td>
+                <td>{blog.description}</td>
+                <td>{blog.city}</td>
+
+                <td>
+                  <UpdatePlaceModal
+                    id={blog._id}
+                    title={blog.title}
+                    description={blog.description}
+                    city={blog.city}
+                    photo={blog.image}
+                  />
+                  <Button
+                    className="btn btn-danger ms-1"
+                    onClick={() => {
+                      handledelete(blog._id);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </div>
-  )
+  );
 }
